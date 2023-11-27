96be90dc997016e9fb5635e253e371cc4e2d7911
Romeo <ralonsoad@gmail.com>
2023-11-27 01:02:50 +0100

    Configure the conexion with db, and install and protect with dotenv

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 import  Sequelize  from "sequelize";
+import dotenv from "dotenv";
+dotenv.config({path: '.env'});
 
 //create a new instance of sequelize
-const db = new Sequelize('realestate_mvc','root','prometheus!',{
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
 
-  host:'localhost',
+  host:process.env.BD_HOST,
   port: 3306,
   dialect: 'mysql',
   define: {
