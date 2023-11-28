038d728b2f4ec0b29afbf81f60301f852faa4447
Romeo <ralonsoad@gmail.com>
2023-11-28 20:36:18 +0100

    Before validate duplicate users

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,8 @@ const loginForm = (req,res) => {
 }
 const signinForm = (req,res) => {
   res.render('auth/signin',{
-    page: 'Please Sign in'
+    page: 'Please Sign in',
+    user: {} // Ensure that the user object is defined, even if it's empty
   })
 }
 const resetPasswordForm = (req,res) => {
@@ -20,12 +21,23 @@ const resetPasswordForm = (req,res) => {
 // create a new recod
 const register = async (req,res) => {
 
-  //validation
-  await check('name').notEmpty().withMessage('Name field can not be empty').run(req)
-  await check('email').isEmail().withMessage('Please enter a valid email address').run(req)
-  await check('password').isLength({ min:6}).withMessage('Password  must be at least 6 chars!').run(req)
-  await check('confirm_password').equals('password').withMessage('Passwords do NOT match!').run(req)
 
+// Validation
+await check('name').notEmpty().withMessage('Name field can not be empty').run(req);
+await check('email').isEmail().withMessage('Please enter a valid email address').run(req);
+await check('password')
+  .isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+  .matches(/\d/).withMessage('Password must contain a number') // Add any additional password requirements here
+  .run(req);
+
+await check('confirm_password')
+  .custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords do not match');
+    }
+    return true;
+  })
+  .run(req);
 
   let result = validationResult(req)
 
@@ -37,7 +49,11 @@ const register = async (req,res) => {
 // if result it is not empty, means that there are some errors
       return res.render('auth/signin',{
         page: 'Please Sign in',
-        errors: result.array()
+        errors: result.array(),
+        user: {
+          name: req.body.name,
+          email: req.body.email
+        }
       })
   }
 
