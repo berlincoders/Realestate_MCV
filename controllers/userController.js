81a58d70be5bf1c8208a5a3df2ded9393b099078
Romeo <ralonsoad@gmail.com>
2023-11-28 14:01:52 +0100

    Validate message with express-validator 41

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,17 +11,36 @@ const signinForm = (req,res) => {
     page: 'Please Sign in'
   })
 }
+const resetPasswordForm = (req,res) => {
+  res.render('auth/reset-password',{
+    page: 'Please Reset Password'
+  })
+}
+
+// create a new recod
 const register = async (req,res) => {
 
   //validation
-  await check('name').notEmpty().withMessage('Required field').run(req)
-  await check('email').isEmail().withMessage('it must be an email adress').run(req)
+  await check('name').notEmpty().withMessage('Name field can not be empty').run(req)
+  await check('email').isEmail().withMessage('Please enter a valid email address').run(req)
+  await check('password').isLength({ min:6}).withMessage('Password  must be at least 6 chars!').run(req)
+  await check('confirm_password').equals('password').withMessage('Passwords do NOT match!').run(req)
 
 
   let result = validationResult(req)
 
 
-  res.json(result.array());
+ // return res.json({errors: result.array()})
+
+  // verify the result is empty.
+  if (!result.isEmpty()) {
+// if result it is not empty, means that there are some errors
+      return res.render('auth/signin',{
+        page: 'Please Sign in',
+        errors: result.array()
+      })
+  }
+
 
   const user = await User.create(req.body)
 
@@ -29,11 +48,8 @@ const register = async (req,res) => {
 
 }
 
-const resetPasswordForm = (req,res) => {
-  res.render('auth/reset-password',{
-    page: 'Please Reset Password'
-  })
-}
+
+
 
 export {
     loginForm,
