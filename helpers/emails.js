05f2e9f1c3378952f16e3771cb1e01abb22ace5f
Romeo <ralonsoad@gmail.com>
2023-11-29 11:59:36 +0100

    Configure Nodemailer & Mailtrap

---
+++ helpers/emails.js
@@ -0,0 +1,18 @@
+import {nodemailer} from 'nodemailer';
+
+
+const emailRecord = async (data) => {
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+          }
+    });
+}
+
+
+export {
+  emailRecord
+}
