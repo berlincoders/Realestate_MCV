adf07e60f71feda8cd6cb2b1180eb8ef48de4333
Romeo <ralonsoad@gmail.com>
2023-12-22 12:54:32 +0100

    Adding the Map

--- src/js/map.js
+++ src/js/map.js
@@ -1,1 +1,13 @@
-alert ('Im the Map')
+(function() {
+
+  const lat = 52.5220823;
+  const lng = 13.413397;
+  const mapa = L.map('mapa').setView([lat, lng ], 12);
+
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mapa);
+
+
+})()
