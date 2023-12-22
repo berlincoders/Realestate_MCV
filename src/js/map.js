f3e27f16cafbe6e6fd6c5904fb56402a6f199722
Romeo <ralonsoad@gmail.com>
2023-12-22 16:40:54 +0100

    Pinpointing

--- src/js/map.js
+++ src/js/map.js
@@ -1,21 +1,26 @@
-(function() {
 
+(function () {
   const lat = 52.5220823;
   const lng = 13.413397;
-  const mapa = L.map('mapa').setView([lat, lng ], 12);
+  const mapa = L.map('mapa').setView([lat, lng], 12);
   let marker;
 
-
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mapa);
 
   // Adding the pin
-  marker = new L.marker([lat, lng],{
+  marker = new L.marker([lat, lng], {
     draggable: true,
     autoPan: true,
   })
-  .addTo(mapa);
+    .addTo(mapa);
 
+  // pintpoint the marker, or pin
+  marker.on('moveend', function (event) {
+    marker = event.target;
 
-})()
+    const position = marker.getLatLng();
+    mapa.panTo(new L.LatLng(position.lat, position.lng));
+  });
+})();
